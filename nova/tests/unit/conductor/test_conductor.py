9fbc0fb38ccbe7bb0b75a214103ecafebfc6de38
Bogdan Popescu <68062990+bopopescu@users.noreply.github.com>
2020-07-24 05:02:36 +0300

    Removing the master/slave vocabulary

--- nova/tests/unit/conductor/test_conductor.py
+++ nova/tests/unit/conductor/test_conductor.py
@@ -2674,7 +2674,7 @@ class ConductorTaskTestCase(_BaseTaskTestCase, test_compute.BaseTestCase):
         get_source_node.assert_called_once_with(
             self.ctxt, instance.host, instance.node)
         get_dest_node.assert_called_once_with(
-            self.ctxt, 'dest-host', use_slave=True)
+            self.ctxt, 'dest-host', use_subordinate=True)
         notify.assert_called_once_with(
             self.ctxt, instance.uuid, 'rebuild_server',
             {'vm_state': instance.vm_state, 'task_state': None}, ex,
@@ -2707,7 +2707,7 @@ class ConductorTaskTestCase(_BaseTaskTestCase, test_compute.BaseTestCase):
         get_source_node.assert_called_once_with(
             self.ctxt, instance.host, instance.node)
         get_dest_node.assert_called_once_with(
-            self.ctxt, 'dest-host', use_slave=True)
+            self.ctxt, 'dest-host', use_subordinate=True)
         claim.assert_called_once_with(
             self.conductor.scheduler_client.reportclient, instance,
             get_source_node.return_value, get_dest_node.return_value)
